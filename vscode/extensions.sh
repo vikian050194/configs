2e8b89e5220e0bd350c17565d99e5e80a1840444
Kirill Vinogradov <vikian050194@gmail.com>
2020-03-12 23:31:50 +0300

    Add color themes

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -6,5 +6,9 @@ code --install-extension ms-vscode.azure-account
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-python.python
 
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension akamud.vscode-theme-onelight
+code --install-extension monokai.theme-monokai-pro-vscode
+
 #code --install-extension esbenp.prettier-vscode
 #code --install-extension wix.vscode-import-cost
