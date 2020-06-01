a03b6cd92a20b9cd61668ddad45d7983dc0b7a19
Kirill Vinogradov <vikian050194@gmail.com>
2020-06-01 11:02:41 +0300

    Add svelte and firefoxe debugger to vs code extensions list

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -5,9 +5,12 @@ code --install-extension HookyQR.beautify
 
 code --install-extension ms-python.python
 code --install-extension msjsdiag.debugger-for-chrome
+# code --install-extension firefox-devtools.vscode-firefox-debug
 
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension redhat.vscode-yaml
+
+code --install-extension svelte.svelte-vscode
 
 # code --install-extension ms-azuretools.vscode-azureappservice
 # code --install-extension ms-vscode.azure-account
