4eac1d94315191bc6d2ee4d0501182255c882205
Kirill Vinogradov <vikian050194@gmail.com>
2020-06-12 21:23:24 +0300

    Add fetch script

--- test.sh
+++ test.sh
@@ -1,16 +1,48 @@
+# #!/bin/env bash
+
 # echo "Print message? (y/n)"
 # read -s answer
 # if [ "$answer" == "y" ]; then
 # echo "message"
 # fi
 
-install () {
-    echo "install $1? (y/n) "
-    read answer #-s
+#===============================
 
-    if [ "$answer" == "y" ]; then
-        eval "$2"
-    fi
-}
+# #!/bin/env bash
 
-install "test" "echo 213"
+# install () {
+#     echo "install $1? (y/n) "
+#     read answer #-s
+
+#     if [ "$answer" == "y" ]; then
+#         eval "$2"
+#     fi
+# }
+
+# install "test" "echo 213"
+
+#===============================
+
+#!/bin/env sh
+
+dir="$1"
+
+echo $dir
+
+case $dir in
+  (*z) echo TRUE;;
+  (*) echo FALSE;;
+esac
+
+# #!/bin/env bash
+
+# dir="$1"
+
+# echo $dir
+
+# if [[ $dir == *z ]];
+# then
+# 	echo TRUE;
+# else
+# 	echo FALSE;
+# fi
