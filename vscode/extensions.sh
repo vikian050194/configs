f270b2c0d6df37539bd42f59dda820fb35dae76c
Kirill Vinogradov <vikian050194@gmail.com>
2020-07-22 15:54:26 +0300

    Add few code extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,29 +1,40 @@
+# linting
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 
+# formatting
 code --install-extension HookyQR.beautify
+# code --install-extension esbenp.prettier-vscode
 
+# languages
 code --install-extension ms-python.python
 # code --install-extension ms-vscode.cpptools
 # code --install-extension ms-dotnettools.csharp
-# code --install-extension ms-vscode.go
+# code --install-extension golang.Go
+# code --install-extension Dart-Code.dart-code
+# code --install-extension Dart-Code.flutter
 
+# frontend debug
 code --install-extension msjsdiag.debugger-for-chrome
 # code --install-extension firefox-devtools.vscode-firefox-debug
 
+# docker
 code --install-extension ms-azuretools.vscode-docker
-code --install-extension redhat.vscode-yaml
+# code --install-extension redhat.vscode-yaml
 
 code --install-extension svelte.svelte-vscode
 
+# azure
 # code --install-extension ms-azuretools.vscode-azureappservice
 # code --install-extension ms-vscode.azure-account
 
 code --install-extension johnpapa.vscode-peacock
 
+# themes
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension akamud.vscode-theme-onelight
 code --install-extension monokai.theme-monokai-pro-vscode
 
-#code --install-extension esbenp.prettier-vscode
-#code --install-extension wix.vscode-import-cost
+
+# code --install-extension ritwickdey.LiveServer
+# code --install-extension wix.vscode-import-cost
