bbf977baa541ab18bc1eea735629bfd69272e7a7
Kirill Vinogradov <vikian050194@gmail.com>
2020-10-03 13:07:03 +0300

    Fix sync script

--- bash/backup/sync.sh
+++ bash/backup/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
 
-rsync --archive --whole-file --verbose --verbose --cvs-exclude --filter='~/git .rsync-filter' --delete --delete-excluded /home/kirill/git /media/data --dry-run
+rsync --archive --whole-file --verbose --verbose --cvs-exclude --filter='. .rsync-filter' --delete --delete-excluded /home/kirill/git /media/data --dry-run
