2caeb10e9fa87087a8d0bfd42857161a17d7d874
Kirill Vinogradov <vikian050194@gmail.com>
2021-04-19 23:17:10 +0300

    add watches limit update script

---
+++ bash/watches.sh
@@ -0,0 +1,9 @@
+echo 32768 | sudo tee /proc/sys/fs/inotify/max_user_watches
+
+# echo fs.inotify.max_user_watches=16384 | sudo tee -a /etc/sysctl.conf
+# sudo sysctl -p
+
+# 8192
+# 16384
+# 32768
+# 65536
