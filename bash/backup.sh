2b6fb5abc3dde398adeff36bf0f34a886ad42efd
Kirill Vinogradov <vikian050194@gmail.com>
2023-05-15 20:54:27 +0200

    update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,25 +1,21 @@
-#!/bin/env sh
+#!/bin/env bash
 
-# sudo mkdir /media/data
-# sudo mount /dev/sdb2 /media/data
+sudo mkdir /media/data
+sudo mount /dev/sdb1 /media/data
 
 SOURCE_DIR="/home/$USER/git"
-echo "from\t$SOURCE_DIR"
 
-TARGET_DIT=$1
-echo "to\t$TARGET_DIT"
+TARGET_DIT="/media/data"
 
 EXCLUDE_FROM="$SOURCE_DIR/.rsync-exclude"
 
 DRY_RUN="--dry-run"
 
-if [ "$2" = "-f" ]; then
+if [ "$1" = "-f" ]; then
     DRY_RUN="" 
 fi
 
-# echo "$DRY_RUN"
-
 rsync --archive --whole-file --progress --verbose --verbose --cvs-exclude --exclude-from $EXCLUDE_FROM --delete --delete-excluded $SOURCE_DIR $TARGET_DIT $DRY_RUN
 
-# sudo umount /media/data
-# sudo rm -rf /media/data
+sudo umount /media/data
+sudo rm -rf /media/data
